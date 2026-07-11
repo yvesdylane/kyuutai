3df2e038326d1f88155ef5ab67d3c2f0b929ef7c
Kanjo-Elkamira-Ndi <kanjoel39@gmail.com>
2026-07-11 04:55:41 +0100

    Feature: Implemented clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import {ClerkProvider, SignInButton, SignUpButton, Show, UserButton} from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +28,20 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ClerkProvider>
+          <header className="flex items-center justify-end gap-4 px-6 py-4 border-b">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton />
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
+          {children}
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
